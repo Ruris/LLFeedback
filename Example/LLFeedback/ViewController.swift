19ac5ddf6b306e59126698936138ce78d2f196a9
Ruris <ruris@foxmail.com>
2024-01-15 11:20:41 +0800

    测试相关信息

--- Example/LLFeedback/ViewController.swift
+++ Example/LLFeedback/ViewController.swift
@@ -10,10 +10,26 @@ import UIKit
 import LLFeedback
 
 class ViewController: UIViewController {
+    
+    struct Section {
+        
+        let title: String
+        
+        let actions: [String]
+    }
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    private let data: [Section] = [
+        Section(title: "自动", actions: ["开始", "重置"]),
+        Section(title: "邮箱", actions: ["发邮件"]),
+        Section(title: "接口", actions: ["Push", "Modal"])
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        LLFeedback.start(true)
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,12 +37,57 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    @IBAction func modalAction(_ sender: Any) {
+    func modalAction() {
         present(UINavigationController(rootViewController: LLFeedback.viewController()), animated: true)
     }
     
-    @IBAction func pushAction(_ sender: Any) {
+    func pushAction() {
         navigationController?.pushViewController(LLFeedback.viewController(), animated: true)
     }
 }
 
+extension ViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data[section].actions.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = data[indexPath.section].actions[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        data[section].title
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        switch (indexPath.section, indexPath.row) {
+        case (0, 0):
+            Task {
+                try? await LLFeedback.feedback(sendTo: "", modalBy: self)
+            }
+        case (0, 1):
+            LLFeedback.reset()
+        case (1, 0):
+            Task {
+                try? await LLFeedback.email(sendTo: "", presentBy: self)
+            }
+        case (2, 0):
+            pushAction()
+        case (2, 1):
+            modalAction()
+        default: break
+        }
+        
+    }
+}
